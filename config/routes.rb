b4703eb8a40b4c1ed8251491a4529ed1425cb24d
Martha Troubh <indexer@icloud.com>
2019-05-06 19:19:12 -0600

    Refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,17 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "home#index"
-  resources :books, only: [:index, :show]
-  resources :authors, only: [:show]
+  resources :books, only: [:index, :show, :new, :create]
+  resources :authors, only: [:show, :new, :create]
   resources :users, only: [:show]
-  resources :books, only: [:new, :create]
-end
+
+  # resources :books, only: [:new, :create, :show] do
+  #   resources :authors, only: [:new, :create]
+  end
+
+
+# Rails.application.routes.draw do
+#   root "home#welcome"
+#   resources :books, only: [:index, :show, :new, :create] do
+#     resources :reviews, only: [:new, :index, :create]
+#   end
