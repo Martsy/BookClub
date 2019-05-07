5463acc1c5c2b10dc51812052129648913b93daa
Martha Troubh <indexer@icloud.com>
2019-05-06 19:19:12 -0600

    Add new and create to books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,9 +7,25 @@ class BooksController < ApplicationController
     @users_most_reviews = User.most_reviews
   end
 
-  def show 
+  def show
     @book = Book.find(params[:id])
     @top_3_reviews = @book.top_3_reviews
     @bottom_3_reviews = @book.bottom_3_reviews
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to "/books/#{book.id}"
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author, :pages)
   end
 end
