a5ea998b3ab8c4a7049e3fd12d5ce0cc8207f3ce
Martha Troubh <indexer@icloud.com>
2019-05-06 19:19:12 -0600

    Revise author contoller

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,22 +1,6 @@
 class AuthorsController < ApplicationController
   def show
     @author = Author.find(params[:id])
-  end
-
-  def new
-    @author = Author.new
-    @book = Book.find(params[:id])
-  end
-
-  def create
-    book = Book.find(params[:id])
-    author = book.authors.create(author_params)
-    redirect_to author_path(author)
-  end
-
-  private
-
-  def author_params
-    params.require(:author).permit(:name)
+    @books = @author.books
   end
 end
