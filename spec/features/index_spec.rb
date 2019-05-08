36c557f333ef0b5737d5017d80b887c7770a2f49
Justin D <justin@baal-software.com>
2019-05-07 22:32:18 -0600

    Add book route, controller, view, and spec files

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -2,9 +2,20 @@ require "rails_helper"
 
 describe "When a user visit / they should see a welcome page" do
 
-  it "should show the home screen" do
-    visit '/'
+  it "should see two links on nav bar" do
 
+    visit '/'
+    within "nav" do
+    expect(page).to have_link "home"
     expect(page).to have_link "books"
+    end
+
+    click_link "books"
+    within "nav" do
+      expect(page).to have_link "home"
+      expect(page).to have_link "books"
+    end
+
   end
+
 end
