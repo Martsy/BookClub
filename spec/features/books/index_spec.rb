998658b900e2435bd05c368dfbfd6c83736eccc4
Justin D <justin@baal-software.com>
2019-05-08 15:57:35 -0600

    Add book test for index page

---
+++ spec/features/books/index_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe "when visiting a book index page" do
+
+  before :each do
+    book_1 = create(:book)
+    book_1.authors << create(:author)
+    book_1.authors << create(:author)
+  end
+
+  it "should show book  info" do
+    visit  books_path
+    save_and_open_page
+    Book.all.each do |book|
+      expect(page).to have_content(book.title)
+      expect(page).to have_content(book.pages)
+      expect(page).to have_content(book.authors.pluck(:name).join(", "))
+      expect(page).to have_content(book.year_published)
+
+    end
+  end
+end
