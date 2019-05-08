ce0a0a05e801fb8854c12b10e6ecf502ead20878
Justin D <justin@baal-software.com>
2019-05-07 22:39:49 -0600

    Correct the test for links on nav baar

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -5,17 +5,12 @@ describe "When a user visit / they should see a welcome page" do
   it "should see two links on nav bar" do
 
     visit '/'
-    within "nav" do
-    expect(page).to have_link "home"
-    expect(page).to have_link "books"
-    end
+
+    click_link "home"
+    expect(current_path).to eq "/"
 
     click_link "books"
-    within "nav" do
-      expect(page).to have_link "home"
-      expect(page).to have_link "books"
-    end
-
+    expect(page).to have_link "home"
   end
 
 end
