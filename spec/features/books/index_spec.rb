5de1562dbe30f91d615a867f6933ccc34c0770eb
Justin D <justin@baal-software.com>
2019-05-08 20:36:08 -0600

    Add test for average rating and number of reviews for books

--- spec/features/books/index_spec.rb
+++ spec/features/books/index_spec.rb
@@ -6,17 +6,22 @@ RSpec.describe "when visiting a book index page" do
     book_1 = create(:book)
     book_1.authors << create(:author)
     book_1.authors << create(:author)
+
+    visit books_path
   end
 
+
   it "should show book  info" do
-    visit  books_path
-
     Book.all.each do |book|
-      expect(page).to have_content(book.title)
-      expect(page).to have_content(book.pages)
-      expect(page).to have_content(book.authors.pluck(:name).join(", "))
-      expect(page).to have_content(book.year_published)
-
+      within "#book-#{book.id}" do
+        expect(page).to have_content(book.title)
+        expect(page).to have_content(book.average_rating)
+        expect(page).to have_content(book.number_of_reviews)
+        expect(page).to have_content(book.pages)
+        expect(page).to have_content(book.authors.pluck(:name).join(", "))
+        expect(page).to have_content(book.year_published)
+        expect(find("img")[:src]).to eq(book.book_cover)
+      end
     end
   end
 end
