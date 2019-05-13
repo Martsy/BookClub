dd7ddb4425d82c088ab40acce472b84e40e3dd19
Justin D <justin@baal-software.com>
2019-05-12 18:39:14 -0600

    Add ability to delete review

--- spec/features/users/user_show_spec.rb
+++ spec/features/users/user_show_spec.rb
@@ -1,18 +1,17 @@
 # coding: utf-8
 require 'rails_helper'
-
 RSpec.describe 'When visiting an user show page' do
 
   before :each do
-    create_list(:review, 400, user: create(:user))
+    create_list(:review, 40, user: create(:user))
     @user = User.first
     visit user_path(@user.id)
   end
 
   it "should have user info" do
     within "#user-show" do
-    expect(page).to have_content @user.name
-    expect(find("img")[:src]).to eq(@user.photo)
+      expect(page).to have_content @user.name
+      expect(find("img")[:src]).to eq(@user.photo)
     end
   end
 
@@ -41,6 +40,17 @@ RSpec.describe 'When visiting an user show page' do
     click_link("sort ascending")
     expect("review-#{Review.first.id}").to appear_before("review-#{Review.last.id}")
 
+  end
+
+  it "should be able to delete a review" do
+    old_review = @user.reviews.first
+    expect(page).to have_content(old_review.headline)
+    within "#review-#{old_review.id}" do
+      expect(page).to have_link("Delete Review")
+      click_link 'Delete Review'
+    end
+    expect(current_path).to eq(user_path(@user))
+    expect(page).to_not have_content(old_review.headline)
   end
 end
 
