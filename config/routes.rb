bff89976839db4b26945753f65b36f8d5a0d3c67
Martha Troubh <indexer@icloud.com>
2019-05-13 21:11:12 -0600

    Add destroy to route
    Co-Authored-by: Justin D <justin@baal-software.com>git

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do # ~> NameError: uninitialized constant Rails
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'home#index'
-  resources :authors, only: [:show]
+  resources :authors, only: [:show, :destroy]
 
   resources :users, only: [:show] do
     resources :reviews, only: [:destroy]
