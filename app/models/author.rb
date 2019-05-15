23d70634d5f406e410b210ba805c8bf95739e3dd
Martha Troubh <indexer@icloud.com>
2019-05-15 01:58:42 -0600

    Add case sensitive validation

--- app/models/author.rb
+++ app/models/author.rb
@@ -3,6 +3,6 @@ class Author < ApplicationRecord
   has_many :books, through: :author_books
 
   validates :name, presence: true
-  validates :name, uniqueness: true
+  validates :name, uniqueness: { case_sensitive: true }
   validates :name, length: { minimum: 2 }
 end
